9c96cb717d843cee96c202a15352a16f99e8aef2
NathaliRodrigues <142436453+NathaliRodrigues@users.noreply.github.com>
2023-08-30 17:21:11 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+//Nathali Rodrigues
+/*
+function imprimirOlaMundo()
+ {console.log("ola mundo !")};
+imprimirOlaMundo()
+
+function imprimirNome(nome)
+{console.log(nome)};
+imprimirNome("Nathali")
+
+let meuNome = "Nathali";
+imprimirNome(meuNome);
+
+const a = 1
+
+function imprimeVariavel ()
+{const b = 2
+console.log(`VAriavel a`, a)
+console.log(`Variavel b`, b)
+}
+
+imprimeVariavel()
+console.log(`Variavel a`, a)
+console.log(`Variavel b`, b)
+
+function calculaArea(altura,largura){
+const area = altura * largura
+return area
+}
+//Atribui retorno á uma variavel
+const areaCalculada = calculaArea(5,8)
+
+//Imprime retorno no console
+console.log(calculaArea(2,3))
+console.log("Variavel areaCalculada: " + areaCalculada)
+console.log("Chamada da função crua: " + calculaArea(5,8))
+
+//exercico 3
+function numeros(n1,n2){
+    const soma = n1 + n2
+    return soma
+}
+const numerosSomados = numeros(5,5)
+console.log(numerosSomados)
+
+
+//exercicio 4
+function divideArray(meuArray){
+let b = [(meuArray[0] /2), (meuArray[meuArray.length-1] /2)];
+return b;
+}
+let a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+const resultado = divideArray(a);
+console.log(resultado)
+
+
+let somaDenumeros = function numeros(n1,n2){
+    const soma = n1 + n2
+    return soma
+}
+const numerosSomados = numeros(5,5)
+console.log(numerosSomados)
+
+*/
